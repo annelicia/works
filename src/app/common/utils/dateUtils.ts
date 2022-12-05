85b6cd850c5d4bdf721507782180c9452c385b75
Annelicia Eunice <anneliciaeunice123@gmail.com>
2022-12-06 01:23:35 +0700

    Change logic toDaysSinceToday.

--- src/app/common/utils/dateUtils.ts
+++ src/app/common/utils/dateUtils.ts
@@ -3,7 +3,7 @@ import { formatDistance } from "date-fns";
 export function toDaysSinceToday(stringDate: string) {
     const distance = formatDistance(new Date(stringDate), new Date());
     if (distance.includes('days')) {
-        return Number(distance.split(' days')[0]) < 7 ? `${distance} ago` : '';
+        return Number(distance.split(' days')[0]) <= 7 ? `${distance} ago` : '';
     } else if (distance.includes('day')) {
         return `${distance} ago`;
     } else {
