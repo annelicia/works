70dfb0d3714658ed4e87c15c19393d97697f62ce
Annelicia Eunice <anneliciaeunice123@gmail.com>
2022-12-06 20:34:13 +0700

    Change logic of get string days since today.
    For the logic, we need to convert date hours to be 0 before compare with date now.

--- src/app/common/utils/dateUtils.ts
+++ src/app/common/utils/dateUtils.ts
@@ -6,14 +6,29 @@ export enum DaysSinceTodayType {
 }
 
 export function toDaysSinceToday(stringDate: string, type: DaysSinceTodayType) {
-    const distance = formatDistance(new Date(stringDate), new Date());
+    const date1 = new Date(stringDate);
+    const dateNow = new Date();
+
+    const distance = formatDistance(date1, dateNow);
     if (distance.includes('hours')) {
         return type === DaysSinceTodayType.JOB_ITEM ? `${distance.replace('hours', 'hrs').replace('about', '')} ago` : `${distance} ago`;
     } else if (distance.includes('days')) {
-        return Number(distance.split(' days')[0]) <= 7 ? `${distance} ago` : '';
+        return getDayDifference(dateNow, date1) <= 7 ? `${distance} ago` : '';
     } else if (distance.includes('day')) {
         return `${distance} ago`;
     } else {
         return '';
     }
+}
+
+function getDayDifference(date1: Date, date2: Date) {
+    date1 = setHoursToZero(date1);
+    date2 = setHoursToZero(date2);
+    return Math.round((date1.getTime() - date2.getTime()) / 86400000);
+}
+
+function setHoursToZero(date: Date) {
+    const tmp = new Date(date);
+    tmp.setHours(0, 0, 0, 0);
+    return tmp;
 }
